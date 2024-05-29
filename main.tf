b5340dd6a3ac6dce3278c178ae3056e9e3257a3f
M3 <m3devbox@gmail.com>
2024-05-28 22:12:00 -0500

    Bump aws to v5.51 and Terraform to v1.8.4

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.51"
     }
 
     cloudflare = {
@@ -11,7 +11,7 @@ terraform {
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.8.4"
 }
 
 provider "aws" {
